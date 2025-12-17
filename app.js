c9c3ea3496b7d28dd0f7c018d2d7112c324f29ad
el73317 <el73317@ubt-uni.net>
2025-12-17 22:43:37 +0100

    pjesa e regjister form me validim permes javascript

--- app.js
+++ app.js
@@ -30,3 +30,67 @@ if(loginForm){
         }
     });
 }
+
+cont regjisterFrom = document.getElementById("registerForm");
+if(regjisterFrom){
+    const emri = document.getElementById("emri");
+    const mbiemri = document.getElementById("mbiemri");
+    const emailReg = document.getElementById("email");
+     const date = document.getElementById("date");
+    const emriError = document.getElementById("emriError");
+    const mbiemriError = document.getElementById("mbiemriError");
+    const emailError = document.getElementById("emailError");
+    const passwordError = document.getElementById("passwordError");
+    const dateError = document.getElementById("dateError");
+    const gjiniaError = document.getElementById("gjiniaError");
+    const formSuccess = document.getElementById("formSuccess");
+
+    const nameRe = /^[A-Za-zÇçËë\s]{4,}$/;
+    const emailRe = /^[^\s@]+@[^\s@]+\.[^\s@]{4,}$/;
+    const passwordRe = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+    regjisterFrom.addEventListener("submit", fuction(e)){
+        e.preventDefault();
+        emriError.textContent = "";
+        mbiemriError.textContent = "";
+        emailError.textContent = "";
+        passwordError.textContent = "";
+        dateError.textContent = "";
+        gjiniaError.textContent = "";
+        formSuccess.textContent = "";
+
+         let valid = true;
+        if(!nameRe.test(emri.value.trim())){
+            emriError.textContent = "Emri i pavlefshëm (min 2 shkronja)";
+            valid = false;
+        }
+        if(!nameRe.test(mbiemri.value.trim())){
+            mbiemriError.textContent = "Mbiemri i pavlefshëm (min 2 shkronja)";
+            valid = false;
+        }
+        if(!emailRe.test(emailReg.value.trim())){
+            emailError.textContent = "Email i pavlefshëm";
+            valid = false;
+        }
+        if(!passwordRe.test(passwordReg.value)){
+            passwordError.textContent = "Password: min 8 karaktere, 1 shkronjë e madhe dhe 1 numër";
+            valid = false;
+        }
+        if(!date.value){
+            dateError.textContent = "Zgjidh datëlindjen";
+            valid = false;
+        }
+        const genderChecked = document.querySelector('input[name="gjinia"]:checked');
+        if(!genderChecked){
+            gjiniaError.textContent = "Zgjidh gjininë";
+            valid = false;
+        }
+
+        if(valid){
+            formSuccess.textContent = "Regjistrimi u krye me sukses! (Demo front-end)";
+            registerForm.reset();
+        }
+    });
+}
+
+

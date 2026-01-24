007b6c878f211001eca2dfabce4502c23ead8597
br73808 <br73808@ubt-uni.net>
2026-01-24 10:20:34 +0100

    Ndryshime

--- produktet.php
+++ produktet.php
@@ -1,27 +1,7 @@
 <?php
+    require_once 'header.php'
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ElektroHome</title>
-    <link rel="stylesheet" href="index.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-</head>
-<body>
-    <nav class="navbar">
-        <div class="logo">ElektroHome</div>
-        <ul class="menu">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">Rreth Nesh</a></li>
-            <li><a href="produktet.html" class="active">Produktet</a></li>
-            <li><a href="kontakt.html">Kontakt</a></li>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="shporta.html"><i class="fa-solid fa-cart-shopping"></i></a></li>
-        </ul>
-    </nav>
 
     <section class="produktet-section">
         <h2>Produktet tona</h2>
@@ -52,31 +32,6 @@
         </div>
     </section>
 
-    <footer>
-        <div class="footer-top">
-        <div class="pjesa-foterit">
-            <h2>ElektroHome</h2>
-            <p>Zgjidhja juaj për siguri moderne dhe profesionale</p>
-        </div>
-        <div class="pjesa-foterit">
-            <h3>Kontakti</h3>
-            <p><i class="fa-solid fa-envelope"></i> info@elektrahome.com</p>
-            <p><i class="fa-solid fa-phone"></i> +383 44 000 800</p>
-            <p><i class="fa-solid fa-location-dot"></i> Ferizaj,Kosovë</p>
-        </div>
-        <div class="pjesa-foterit">
-            <h3>Na ndiqni</h3>
-            <div class="social-icons">
-                <a href="#"><i class="fab fa-facebook"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-tiktok"></i></a>
-            </div>
-        </div>
-    </div>
-        <div class="pjesa-foteritt">
-            © 2025 ElektroHome. Të gjitha të drejtat e rezervuara
-        </div>
-    </footer>
-    
-</body>
-</html>
+   <?php
+    require_once 'footer.php'
+?>
--- shporta.php
+++ shporta.php
@@ -1,24 +1,15 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ElektroHomet</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
-<body>
-    <nav class="navbar">
-        <div class="logo">ElektroHome</div>
-        <ul class="menu">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">Rreth Nesht</a></li>
-            <li><a href="produktet.html">Produktet</a></li>
-            <li><a href="login.html">Login</a></li>
-        </ul>
-    </nav>
-
+<?php
+    require_once 'header.php'
+?>
     <section class="shporta-section">
         <h2>Shporta ime</h2>
         <div class="shporta-container">

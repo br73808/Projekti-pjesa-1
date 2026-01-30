e064d0a708dfe29deb710e447ebbf4ce448fe757
br73808 <br73808@ubt-uni.net>
2026-01-31 00:53:14 +0100

    !

--- JS/app.js
+++ JS/app.js
@@ -124,7 +124,7 @@ if (contactForm) {
   contactForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    // Clear errors
+
     nameError.textContent = "";
     emailError.textContent = "";
     subjectError.textContent = "";
@@ -185,30 +185,3 @@ function prevSlide() {
     }
     showSlide(index);
 }
-
-let slides = document.querySelectorAll(".slide");
-let index = 0;
-
-function showSlide(i){
-    slides.forEach(slide => slide.classList.remove("active"));
-    slides[i].classList.add("active");
-}
-
-function nextSlide(){
-    if(index < slides.length -1){
-        index++;
-    }else{
-        index = 0;
-    }
-    showSlide(index);
-}
-
-function prevSlide() {
-    if(index > 0){
-        index--;
-    }else{
-        index = slides.length -1;
-    }
-    showSlide(index);
-}
-

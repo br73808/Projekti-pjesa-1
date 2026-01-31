069efbe10b60c25a3dae28f09ad7417341095c50
el73317 <el73317@ubt-uni.net>
2026-02-01 00:40:53 +0100

    Permisime ne header.php

--- HTML/header.php
+++ HTML/header.php
@@ -1,5 +1,4 @@
 <?php
-
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
@@ -25,10 +24,15 @@ if (session_status() == PHP_SESSION_NONE) {
             <li><a href="kontakt.php">Kontakt</a></li>
 
             <?php if(isset($_SESSION['user_id'])): ?>
-               
+
+                <?php if(isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == 1): ?>
+                
+                    <li><a href="admin/dashboard.php">Dashboard</a></li>
+                <?php endif; ?>
+            
                 <li><a href="logout.php">Logout</a></li>
             <?php else: ?>
-             
+                
                 <li><a href="login.php">Login</a></li>
             <?php endif; ?>
 

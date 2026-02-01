9e1f51241703d284837d9aa39bd5a1a689e24bbf
br73808 <br73808@ubt-uni.net>
2026-02-01 01:33:27 +0100

    Shtimi i mesazhit: Regjistrimi u krye me sukses! Kyçu tani ✅

--- HTML/login.php
+++ HTML/login.php
@@ -20,23 +20,23 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-       if ($user && password_verify($password, $user['password'])) {
-    $_SESSION['user_id'] = $user['user_id'];
-    $_SESSION['isAdmin'] = $user['isAdmin'];
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['user_id'];
+            $_SESSION['isAdmin'] = $user['isAdmin'];
 
-    if ((int)$user['isAdmin'] === 1) {
-        header("Location: admin/dashboard.php");
-    } else {
-        header("Location: index.php");
-    }
-    exit;
-} else {
-    $error = "Email ose password i pasaktë.";
-}
-
+            if ((int)$user['isAdmin'] === 1) {
+                header("Location: admin/dashboard.php");
+            } else {
+                header("Location: index.php");
+            }
+            exit;
+        } else {
+            $error = "Email ose password i pasaktë.";
+        }
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="sq">
 <head>
@@ -52,6 +52,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <div class="card">
     <h2>Kyçu</h2>
 
+
+    <?php if (isset($_GET['registered'])): ?>
+        <p style="color: green; text-align:center;">
+            Regjistrimi u krye me sukses! Kyçu tani ✅
+        </p>
+    <?php endif; ?>
+
+    
     <?php if (!empty($error)): ?>
         <p class="error" style="text-align:center;"><?= $error ?></p>
     <?php endif; ?>
@@ -70,5 +78,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         </p>
     </form>
 </div>
+
 </body>
 </html>
